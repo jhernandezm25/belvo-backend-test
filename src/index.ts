89dadebeaef647d5ddb9d34288a63ce777a02c2c
jhernandezm2 <jhernandezm@acid.cl>
2023-12-18 08:23:20 -0500

    :star: first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
